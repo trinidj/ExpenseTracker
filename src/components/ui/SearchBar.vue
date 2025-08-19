64dd3663a985425d0f6602d3560ca0e9c65ab3d1
trinidj <jm.trinidad199@gmail.com>
2025-08-19 15:29:33 -0400

    feat: add SearchBar UI component with search icon and input field

---
+++ src/components/ui/SearchBar.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import { Search } from 'lucide-vue-next';
+</script>
+
+<template>
+  <div class="w-full bg-[#888888]/27 flex flex-row items-center gap-2 p-2 rounded-md">
+    <Search 
+      class="text-[#888888]"
+      :size="20"
+    />
+    <input 
+      type="text" 
+      placeholder="Search"
+      class="w-full focus:outline-0"  
+    />
+  </div>
+</template>
