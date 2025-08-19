9139b57ce68aefbb1111f447a3a3ff2e12134765
trinidj <jm.trinidad199@gmail.com>
2025-08-19 15:05:26 -0400

    fix: correct PrimeView theme import path from @primeuix/themes to @primeuix/themes/aura

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './style.css'
 import App from './App.vue'
 import router from '@/router';
 import PrimeVue from 'primevue/config';
-import Aura from '@primeuix/themes';
+import Aura from '@primeuix/themes/aura';
 
 const app = createApp(App);
 app.use(router);
