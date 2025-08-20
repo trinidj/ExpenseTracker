6af3e47193539f5effc0ef864a3720e3e6fa992d
trinidj <jm.trinidad199@gmail.com>
2025-08-19 20:28:09 -0400

    feat: add active state styling to navbar navigation links

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,20 +1,26 @@
 <script setup>
+  import { ref } from 'vue';
+
   import { Plus, Home, ArrowLeftRight, ChartArea, Banknote, } from 'lucide-vue-next';
   import { RouterLink } from 'vue-router';
+
+  const isActive = ref(false);
 </script>
 
 <template>
   <nav class="fixed bottom-0 h-auto left-0 right-0 bg-white border-t-2 border-t-teal-400 rounded-tr-3xl rounded-tl-3xl px-10 py-4 ">
     <div class="flex flex-row items-center justify-between relative">
-      <RouterLink to="/">
+      <RouterLink to="/" v-slot="{ isActive }">
         <Home 
           :size="25"
+          :class="isActive ? 'text-emerald-500' : 'text-[#888888]'"
         />
       </RouterLink>
 
-      <RouterLink to="/transactions">
+      <RouterLink to="/transactions" v-slot="{ isActive }">
         <ArrowLeftRight 
           :size="25"
+          :class="isActive ? 'text-emerald-500' : 'text-[#888888]'"
         />
       </RouterLink>
 
@@ -25,12 +31,14 @@
       <button>
         <ChartArea 
           :size="25"
+          class="text-[#888888]"
         />
       </button>
 
       <button>
         <Banknote
           :size="25"
+          class="text-[#888888]"
         />
       </button>
     </div>
