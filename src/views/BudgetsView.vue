6ac1577905819e01c688d2b0dbb706efbf128bd0
trinidj <jm.trinidad199@gmail.com>
2025-08-20 22:11:42 -0400

    feat: add initial BudgetsView component

---
+++ src/views/BudgetsView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <header class="flex flex-row justify-between items-center p-6">
+    <h1 class="text-2xl font-semibold static">Budgets</h1>
+  </header>
+</template>
