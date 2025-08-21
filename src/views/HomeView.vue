9b2ca3b44b041d05c5d607c35bf60ff279d7cc54
trinidj <jm.trinidad199@gmail.com>
2025-08-21 13:52:56 -0400

    style: improve Home view UI with enhanced balance card and simplified transactions list

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,26 +6,26 @@
 
 <template>
   <div class="bg-[#FFE3C6] h-62 rounded-bl-3xl rounded-br-3xl">
-    <!-- Header -->
-    <header class="flex flex-row justify-between items-center p-6">
-      <h1 class="text-2xl font-semibold static">Home</h1>
+    <!-- Header -->                                           
+    <header class="flex flex-row justify-start items-center px-6 py-8">
+      <h1 class="text-zinc-800 text-2xl font-semibold">Home</h1>
     </header>
     
     <!-- Balance Card -->
-    <section class="static flex flex-col bg-gradient-to-b from-teal-300 to-emerald-300 rounded-xl mx-6 shadow-black/20 shadow-2xl gap-5 justify-between">
-      <div class="flex flex-col p-5 gap-1">
+    <section class="static flex flex-col bg-gradient-to-b from-emerald-300 to-[#6EE7DB] rounded-xl mx-6 shadow-black/20 shadow-2xl gap-5 justify-between">
+      <div class="flex flex-col p-5 gap-1 text-gray-50">
         <div class="flex flex-row items-center gap-1">
           <DollarSign 
             :size="16"
           />
-          <p>Total Balance</p>
+          <p class="">Total Balance</p>
         </div>
 
-        <p class="font-bold text-3xl">$3,000.00</p>
+        <p class="font-medium text-3xl">$3,000.00</p>
       </div>
 
       <div class="flex flex-row items-center justify-between p-5 pt-0">
-        <div class="flex flex-col gap-1">
+        <div class="flex flex-col gap-1 text-gray-50">
           <div class="flex flex-row items-center gap-1">
             <ArrowDown 
               :size="16"
@@ -33,10 +33,10 @@
             <p>Income</p>
           </div>
 
-          <p class="font-semibold text-2xl">$500.00</p>
+          <p class="font-medium text-2xl">$500.00</p>
         </div>
 
-        <div class="flex flex-col gap-1">
+        <div class="flex flex-col gap-1 text-gray-50">
           <div class="flex flex-row items-center gap-1">
             <ArrowUp 
               :size="16"
@@ -44,7 +44,7 @@
             <p>Expenses</p>
           </div>
 
-          <p class="font-bold text-2xl">$500.00</p>
+          <p class="font-medium text-2xl">$500.00</p>
         </div>
       </div>
     </section>
@@ -52,97 +52,32 @@
     <!-- Recent Transactions -->
     <section>
       <header class="flex flex-row items-center justify-between m-6 mb-5">
-        <h2 class="text-lg">Recent Transactions</h2>
+        <h2 class="text-lg text-zinc-900">Recent Transactions</h2>
         <RouterLink to="/transactions">
           <p class="text-[#000000]/50 font-balance text-sm hover:underline">View All</p>
         </RouterLink>
       </header>
 
-      <ul class="flex flex-col mx-6 gap-8">
+      <ul class="flex flex-col mx-6 gap-6">
         <li class="flex flex-row items-center justify-between">
           <div class="flex items-center gap-2">
             <div class="bg-emerald-300 p-2 rounded-md">
               <Clapperboard 
-                class="text-emerald-600"
+                :size="20"
+                class="text-emerald-100"
               />
             </div>
             <div class="flex flex-col">
-              <h3 class="text-lg">Netflix</h3>
-              <p class="text-black/45 text-sm">Entertainment</p>
+              <h3 class="text-base">Netflix</h3>
+              <p class="text-black/45 text-xs">Entertainment</p>
             </div>
           </div>
-          <p class="text-lg font-semibold">
+          <p class="text-base font-medium text-red-400">
             -$20.00
           </p>
         </li>
 
-        <li class="flex flex-row items-center justify-between">
-          <div class="flex items-center gap-2">
-            <div class="bg-emerald-300 p-2 rounded-md">
-              <Clapperboard 
-                class="text-emerald-600"
-              />
-            </div>
-            <div class="flex flex-col">
-              <h3 class="text-lg">Netflix</h3>
-              <p class="text-black/45 text-sm">Entertainment</p>
-            </div>
-          </div>
-          <p class="text-lg font-semibold">
-            -$20.00
-          </p>
-        </li>
-
-        <li class="flex flex-row items-center justify-between">
-          <div class="flex items-center gap-2">
-            <div class="bg-emerald-300 p-2 rounded-md">
-              <Clapperboard 
-                class="text-emerald-600"
-              />
-            </div>
-            <div class="flex flex-col">
-              <h3 class="text-lg">Netflix</h3>
-              <p class="text-black/45 text-sm">Entertainment</p>
-            </div>
-          </div>
-          <p class="text-lg font-semibold">
-            -$20.00
-          </p>
-        </li>
-
-        <li class="flex flex-row items-center justify-between">
-          <div class="flex items-center gap-2">
-            <div class="bg-emerald-300 p-2 rounded-md">
-              <Clapperboard 
-                class="text-emerald-600"
-              />
-            </div>
-            <div class="flex flex-col">
-              <h3 class="text-lg">Netflix</h3>
-              <p class="text-black/45 text-sm">Entertainment</p>
-            </div>
-          </div>
-          <p class="text-lg font-semibold">
-            -$20.00
-          </p>
-        </li>
-
-        <li class="flex flex-row items-center justify-between">
-          <div class="flex items-center gap-2">
-            <div class="bg-emerald-300 p-2 rounded-md">
-              <Clapperboard 
-                class="text-emerald-600"
-              />
-            </div>
-            <div class="flex flex-col">
-              <h3 class="text-lg">Netflix</h3>
-              <p class="text-black/45 text-sm">Entertainment</p>
-            </div>
-          </div>
-          <p class="text-lg font-semibold">
-            -$20.00
-          </p>
-        </li>
+        
       </ul>
     </section>
   </div>
