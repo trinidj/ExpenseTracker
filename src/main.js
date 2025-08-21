d2b55ac8e4944610809fafc1fea7d27c64ca2221
trinidj <jm.trinidad199@gmail.com>
2025-08-20 22:49:50 -0400

    feat: add KeyFilter directive for input validation

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import App from './App.vue'
 import router from '@/router';
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
+import { KeyFilter } from 'primevue';
 
 const app = createApp(App);
+app.directive('keyfilter', KeyFilter);
 app.use(router);
 app.use(PrimeVue, {
   theme: {
