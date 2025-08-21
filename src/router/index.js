5fd538701ac4d7d3a3a96defd25673fc11888a56
trinidj <jm.trinidad199@gmail.com>
2025-08-20 22:17:53 -0400

    feat: add budgets route to router config

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,13 @@ import { createMemoryHistory, createRouter } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import TransactionsView from "@/views/TransactionsView.vue";
 import AnalyticsView from "@/views/AnalyticsView.vue";
+import BudgetsView from "@/views/BudgetsView.vue";
 
 const routes = [
   { path: '/', component: HomeView },
   { path: '/transactions', component: TransactionsView },
   { path: '/analytics', component: AnalyticsView },
+  { path: '/budgets', component: BudgetsView },
 ];
 
 const router = createRouter({
