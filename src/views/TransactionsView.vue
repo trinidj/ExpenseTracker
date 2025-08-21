d3f3970aa61e0483614891ec992f5fafb0b96fe4
trinidj <jm.trinidad199@gmail.com>
2025-08-21 15:25:16 -0400

    style: refine UI consistency across HomeView and TransactionsView

--- src/views/TransactionsView.vue
+++ src/views/TransactionsView.vue
@@ -6,37 +6,39 @@
 <template>
   <div class="h-screen flex flex-col overflow-hidden">
     <!-- Header -->
-    <header class="flex flex-row justify-between items-center p-6">
-      <h1 class="text-2xl font-semibold static">Transactions</h1>
+    <header class="flex flex-row justify-start items-center px-6 py-8">
+      <h1 class="text-zinc-800 text-2xl font-semibold">Transactions</h1>
     </header>
     
-    <section class="mx-6 flex flex-row items-center gap-3 mb-8 flex-shrink-0">
+    <div class="mx-6 flex flex-row items-center gap-3 mb-8 flex-shrink-0">
       <SearchBar />
+      
       <button class="cursor-pointer bg-emerald-300 p-2 rounded-md text-white hover:bg-emerald-400 transition duration-200 ease-in-out">
         <Filter />
       </button>
-    </section>
+    </div>
 
     <!-- Transactions -->
     <ScrollPanel class="flex! flex-col! gap-8! overflow-y-auto! flex-1! pb-20!">
-      <section class="flex flex-col gap-5"> 
+      <section class="flex flex-col gap-6"> 
         <header>
-          <h2 class="flex items-center justify-center text-black/50">Today</h2>
+          <h2 class="flex items-center justify-center text-black/50 text-xs">Today</h2>
         </header>
         <ul class="flex flex-col mx-6 gap-8">
           <li class="flex flex-row items-center justify-between">
             <div class="flex items-center gap-2">
               <div class="bg-emerald-300 p-2 rounded-md">
                 <Clapperboard 
-                  class="text-emerald-600"
+                  :size="20"
+                  class="text-emerald-100"
                 />
               </div>
               <div class="flex flex-col">
-                <h3 class="text-lg">Netflix</h3>
-                <p class="text-black/45 text-sm">Entertainment</p>
+                <h3 class="text-base">Netflix</h3>
+                <p class="text-black/45 text-xs">Entertainment</p>
               </div>
             </div>
-            <p class="text-lg font-semibold">
+            <p class="text-base font-medium text-red-400">
               -$20.00
             </p>
           </li>
@@ -45,15 +47,16 @@
             <div class="flex items-center gap-2">
               <div class="bg-emerald-300 p-2 rounded-md">
                 <Clapperboard 
-                  class="text-emerald-600"
+                  :size="20"
+                  class="text-emerald-100"
                 />
               </div>
               <div class="flex flex-col">
-                <h3 class="text-lg">Netflix</h3>
-                <p class="text-black/45 text-sm">Entertainment</p>
+                <h3 class="text-base">Netflix</h3>
+                <p class="text-black/45 text-xs">Entertainment</p>
               </div>
             </div>
-            <p class="text-lg font-semibold">
+            <p class="text-base font-medium text-red-400">
               -$20.00
             </p>
           </li>
@@ -62,39 +65,41 @@
             <div class="flex items-center gap-2">
               <div class="bg-emerald-300 p-2 rounded-md">
                 <Clapperboard 
-                  class="text-emerald-600"
+                  :size="20"
+                  class="text-emerald-100"
                 />
               </div>
               <div class="flex flex-col">
-                <h3 class="text-lg">Netflix</h3>
-                <p class="text-black/45 text-sm">Entertainment</p>
+                <h3 class="text-base">Netflix</h3>
+                <p class="text-black/45 text-xs">Entertainment</p>
               </div>
             </div>
-            <p class="text-lg font-semibold">
+            <p class="text-base font-medium text-red-400">
               -$20.00
             </p>
           </li>
         </ul>
       </section>
 
-      <section class="flex flex-col gap-5"> 
+      <section class="flex flex-col gap-6"> 
         <header>
-          <h2 class="flex items-center justify-center text-black/50">Yesterday</h2>
+          <h2 class="flex items-center justify-center text-black/50 text-xs">Yesterday</h2>
         </header>
         <ul class="flex flex-col mx-6 gap-8">
           <li class="flex flex-row items-center justify-between">
             <div class="flex items-center gap-2">
               <div class="bg-emerald-300 p-2 rounded-md">
                 <Clapperboard 
-                  class="text-emerald-600"
+                  :size="20"
+                  class="text-emerald-100"
                 />
               </div>
               <div class="flex flex-col">
-                <h3 class="text-lg">Netflix</h3>
-                <p class="text-black/45 text-sm">Entertainment</p>
+                <h3 class="text-base">Netflix</h3>
+                <p class="text-black/45 text-xs">Entertainment</p>
               </div>
             </div>
-            <p class="text-lg font-semibold">
+            <p class="text-base font-medium text-red-400">
               -$20.00
             </p>
           </li>
@@ -103,15 +108,16 @@
             <div class="flex items-center gap-2">
               <div class="bg-emerald-300 p-2 rounded-md">
                 <Clapperboard 
-                  class="text-emerald-600"
+                  :size="20"
+                  class="text-emerald-100"
                 />
               </div>
               <div class="flex flex-col">
-                <h3 class="text-lg">Netflix</h3>
-                <p class="text-black/45 text-sm">Entertainment</p>
+                <h3 class="text-base">Netflix</h3>
+                <p class="text-black/45 text-xs">Entertainment</p>
               </div>
             </div>
-            <p class="text-lg font-semibold">
+            <p class="text-base font-medium text-red-400">
               -$20.00
             </p>
           </li>
@@ -120,39 +126,41 @@
             <div class="flex items-center gap-2">
               <div class="bg-emerald-300 p-2 rounded-md">
                 <Clapperboard 
-                  class="text-emerald-600"
+                  :size="20"
+                  class="text-emerald-100"
                 />
               </div>
               <div class="flex flex-col">
-                <h3 class="text-lg">Netflix</h3>
-                <p class="text-black/45 text-sm">Entertainment</p>
+                <h3 class="text-base">Netflix</h3>
+                <p class="text-black/45 text-xs">Entertainment</p>
               </div>
             </div>
-            <p class="text-lg font-semibold">
+            <p class="text-base font-medium text-red-400">
               -$20.00
             </p>
           </li>
         </ul>
       </section>
 
-      <section class="flex flex-col gap-5"> 
+      <section class="flex flex-col gap-6"> 
         <header>
-          <h2 class="flex items-center justify-center text-black/50">Yesterday</h2>
+          <h2 class="flex items-center justify-center text-black/50 text-xs">Two Dats Ago</h2>
         </header>
         <ul class="flex flex-col mx-6 gap-8">
           <li class="flex flex-row items-center justify-between">
             <div class="flex items-center gap-2">
               <div class="bg-emerald-300 p-2 rounded-md">
                 <Clapperboard 
-                  class="text-emerald-600"
+                  :size="20"
+                  class="text-emerald-100"
                 />
               </div>
               <div class="flex flex-col">
-                <h3 class="text-lg">Netflix</h3>
-                <p class="text-black/45 text-sm">Entertainment</p>
+                <h3 class="text-base">Netflix</h3>
+                <p class="text-black/45 text-xs">Entertainment</p>
               </div>
             </div>
-            <p class="text-lg font-semibold">
+            <p class="text-base font-medium text-red-400">
               -$20.00
             </p>
           </li>
@@ -161,15 +169,16 @@
             <div class="flex items-center gap-2">
               <div class="bg-emerald-300 p-2 rounded-md">
                 <Clapperboard 
-                  class="text-emerald-600"
+                  :size="20"
+                  class="text-emerald-100"
                 />
               </div>
               <div class="flex flex-col">
-                <h3 class="text-lg">Netflix</h3>
-                <p class="text-black/45 text-sm">Entertainment</p>
+                <h3 class="text-base">Netflix</h3>
+                <p class="text-black/45 text-xs">Entertainment</p>
               </div>
             </div>
-            <p class="text-lg font-semibold">
+            <p class="text-base font-medium text-red-400">
               -$20.00
             </p>
           </li>
@@ -178,15 +187,16 @@
             <div class="flex items-center gap-2">
               <div class="bg-emerald-300 p-2 rounded-md">
                 <Clapperboard 
-                  class="text-emerald-600"
+                  :size="20"
+                  class="text-emerald-100"
                 />
               </div>
               <div class="flex flex-col">
-                <h3 class="text-lg">Netflix</h3>
-                <p class="text-black/45 text-sm">Entertainment</p>
+                <h3 class="text-base">Netflix</h3>
+                <p class="text-black/45 text-xs">Entertainment</p>
               </div>
             </div>
-            <p class="text-lg font-semibold">
+            <p class="text-base font-medium text-red-400">
               -$20.00
             </p>
           </li>
