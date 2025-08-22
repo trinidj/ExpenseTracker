fbbbab2ceda665e0dbf1c81e8cf3ec620ed9451d
trinidj <jm.trinidad199@gmail.com>
2025-08-21 21:32:09 -0400

    feat: add new budget dialog with form components to BudgetsView

--- src/views/BudgetsView.vue
+++ src/views/BudgetsView.vue
@@ -1,7 +1,8 @@
 <script setup>
   import { ref } from 'vue';
 
-  import { ScrollPanel, ProgressBar, Button } from 'primevue';
+  import { ScrollPanel, ProgressBar, Button, Dialog, Select } from 'primevue';
+  import { Form } from '@primevue/forms';
 
   import { PiggyBank, Wallet, Plus } from 'lucide-vue-next';
 
@@ -19,16 +20,54 @@
     { name: 'Travel' },
     { name: 'Housing' }
   ]);
+
+  const position = ref('center');
+  const visible = ref(false);
+
+  const openPosition = (pos) => {
+      position.value = pos;
+      visible.value = true;
+  };
 </script>
 
 <template>
   <div class="bg-[#FFE3C6] h-62 rounded-bl-3xl rounded-br-3xl">
     <header class="flex flex-row justify-start items-center px-6 py-8">
       <h1 class="text-zinc-800 text-2xl font-semibold">Budgets</h1>
-      <
     </header>
 
-     <ScrollPanel>
+    <Dialog
+      modal
+      header="New Budget"
+      v-model:visible="visible"  
+      :position="position" 
+      :draggable="false"
+    >
+      <Form>
+        <div class="flex items-center gap-4 col-span-full">
+          <SelectButton 
+            fluid
+            v-model="selectButtonValue"
+            :options="selectButtonOptions"
+          />
+        </div>
+
+        <div class="flex flex-col gap-2">
+          <InputGroup>
+            <InputGroupAddon class="flex! items-center! justify-center!">
+              <DollarSign 
+                :size="20"
+              />
+            </InputGroupAddon>
+            
+            <InputText v-keyfilter.money placeholder="Price" size="small" fluid />
+          </InputGroup>
+        </div>
+      </Form>
+    </Dialog>
+
+
+    <ScrollPanel>
       <div class="flex flex-col gap-5">
         <!-- Budget Progress -->
         <div class="bg-white rounded-xl mx-6 gap-5 border border-emerald-300/50">
@@ -62,7 +101,12 @@
               <h2 class="font-medium text-zinc-800">Category Budgets</h2>
             </div>
 
-            <Button unstyled type="button" label="Add">
+            <Button 
+              unstyled 
+              type="button" 
+              label="Add" 
+              @click="openPosition('bottom')"
+            >
               <div class="p-2 bg-emerald-400 rounded-md">
                 <Plus 
                   :size="16"
