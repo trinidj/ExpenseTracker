bdc01f37863cc7d2492141f2c9063d73f5d96390
trinidj <jm.trinidad199@gmail.com>
2025-08-24 17:32:23 -0400

    refactor: simplify transactions store by removing unused categories and methods

--- src/stores/useTransactionsStore.js
+++ src/stores/useTransactionsStore.js
@@ -1,19 +1,9 @@
 import { defineStore } from "pinia";
-import { Clapperboard, Plane, ShoppingCart, Home } from "lucide-vue-next";
 import { ref } from "vue";
 
 export const useTransactionsStore = defineStore('transactions', () => {
-  // state
-  const categories = ref([
-    { name: 'Entertainment', icon: Clapperboard },
-    { name: 'Travel', icon: Plane },
-    { name: 'Shopping', icon: ShoppingCart },
-    { name: 'Home', icon: Home },
-  ]);
-
   const transactions = ref([]);
 
-  // action
   const addTransaction = (transaction) => {
     const newTransaction = {
       id: Date.now(),
@@ -24,18 +14,8 @@ export const useTransactionsStore = defineStore('transactions', () => {
     transactions.value.unshift(newTransaction);
   };
 
-  const updateTransaction = (id, updatedData) => {
-    const index = transactions.value.findIndex(t => t.id === id);
-    if (index > -1) {
-      transactions.value[index] = { ...transactions.value[index], ...updatedData };
-    }
-  };
-
   return { 
-    categories, 
     transactions,
-    getCategoryByName,
     addTransaction,
-    updateTransaction, 
   };
 });
