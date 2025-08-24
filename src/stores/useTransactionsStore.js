b328525aed001d8213ebf258bd1765bef53c9a58
trinidj <jm.trinidad199@gmail.com>
2025-08-24 17:49:21 -0400

    feat: add localStorage persistance to transactions store

--- src/stores/useTransactionsStore.js
+++ src/stores/useTransactionsStore.js
@@ -4,6 +4,14 @@ import { ref } from "vue";
 export const useTransactionsStore = defineStore('transactions', () => {
   const transactions = ref([]);
 
+  const getTransactions = () => {
+    const stored = localStorage.getItem('transactions');
+    
+    if (stored) {
+      transactions.value = JSON.parse(stored);
+    }
+  };
+
   const addTransaction = (transaction) => {
     const newTransaction = {
       id: Date.now(),
@@ -12,10 +20,13 @@ export const useTransactionsStore = defineStore('transactions', () => {
     };
 
     transactions.value.unshift(newTransaction);
+
+    localStorage.setItem('transactions', JSON.stringify(transactions.value));
   };
 
   return { 
     transactions,
+    getTransactions,
     addTransaction,
   };
 });
