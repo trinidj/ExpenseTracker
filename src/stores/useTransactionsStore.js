e6e72e95a4e82e37143c527e0bee3c9f802be73e
trinidj <jm.trinidad199@gmail.com>
2025-08-25 12:38:39 -0400

    feat: create getter to group transactions by their date

--- src/stores/useTransactionsStore.js
+++ src/stores/useTransactionsStore.js
@@ -2,8 +2,28 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 export const useTransactionsStore = defineStore('transactions', () => {
+  // state
   const transactions = ref([]);
 
+  // getters
+  const getTransactionsByDate = (transactions) => {
+    const groupedTransactions = {};
+
+    transactions.forEach(transaction => {
+      const date = new Date(transaction.createdAt);
+      const dateKey = date.toDateString();
+
+      if (!groupedTransactions[dateKey]) {
+        groupedTransactions[dateKey] = [];
+      }
+
+      groupedTransactions[dateKey].push(transaction);
+    });
+
+    return groupedTransactions;
+  }
+    
+  // actions
   const addTransaction = (transaction) => {
     const newTransaction = {
       id: Date.now(),
@@ -16,6 +36,7 @@ export const useTransactionsStore = defineStore('transactions', () => {
 
   return { 
     transactions,
+    getTransactionsByDate,
     addTransaction,
   };
 }, {
