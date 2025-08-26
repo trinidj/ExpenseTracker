1894361b6f6aa1a56f19df3b3d0484adcd76f300
trinidj <jm.trinidad199@gmail.com>
2025-08-25 23:09:58 -0400

    refactor: remove deprecated localStorage utility functions

--- src/utils/saveToStorage.js
+++
@@ -1,7 +0,0 @@
-export const saveToStorage = (storageKey, listItems) => {
-  try {
-    localStorage.setItem(storageKey, JSON.stringify(listItems.value));
-  } catch (error) {
-    console.error('Error saving to localStorage:', error);
-  }
-}
