341ce3e7916e7c92993d1d4453711972e92ffdd5
trinidj <jm.trinidad199@gmail.com>
2025-08-25 23:10:52 -0400

    feat: add budget tracking computes for spent amount and percentage

--- src/stores/useBudgetStore.js
+++ src/stores/useBudgetStore.js
@@ -1,10 +1,26 @@
 import { defineStore } from "pinia";
-import { ref } from "vue";
+import { ref, computed } from "vue";
+import { useTransactionsStore } from '@/stores/useTransactionsStore';
+
 
 export const useBudgetStore = defineStore('budgets', () => {
   // states
   const budgets = ref([]);
   let totalBudget = ref(0);
+
+  // getters
+  const totalSpent = computed(() => {
+    const transactionsStore = useTransactionsStore();
+
+    return transactionsStore.transactions.reduce((total, transaction) => {
+      const amount = transaction.amount || 0;
+      return amount < 0 ? total + Math.abs(amount) : total;
+    }, 0);
+  });
+
+  const budgetSpentPercentage = computed(() => {
+    return totalBudget.value > 0 ? ((totalSpent.value / totalBudget.value) * 100).toFixed(0) : 0;
+  });
 
   // actions
   const addBudgetItem = (budget) => {
@@ -19,6 +35,8 @@ export const useBudgetStore = defineStore('budgets', () => {
   return {
     budgets,
     totalBudget,
+    budgetSpentPercentage,
+    totalSpent,
     addBudgetItem,
   };
 }, {
