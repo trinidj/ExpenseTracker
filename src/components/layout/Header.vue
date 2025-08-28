b8186c4f1105c2649e61abb7ae6503c744f7a38f
trinidj <jm.trinidad199@gmail.com>
2025-08-28 17:52:33 -0400

    feat: add navigation links to Header drawer

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,6 +1,6 @@
 <script setup>
   import { ref } from 'vue';
-  import { Menu } from 'lucide-vue-next';
+  import { Menu, Home, ArrowLeftRight, ChartArea, Banknote  } from 'lucide-vue-next';
   import { RouterLink } from 'vue-router';
   
   import { Drawer } from 'primevue';
@@ -13,6 +13,13 @@
   });
 
   const visible = ref(false);
+
+  const navLinks = [
+    { name: 'Home', path: '/', icon: Home },
+    { name: 'Transactions', path: '/transactions', icon: ArrowLeftRight },
+    { name: 'Analytics', path: '/analytics', icon: ChartArea },
+    { name: 'Budgets', path: '/budgets', icon: Banknote }
+  ]
 </script>
 
 <template>
@@ -22,7 +29,20 @@
       header="Expense Tracker"
       class="border-2! border-r-emerald-300!"
     >
-      
+      <div class="h-full">
+        <nav class="flex flex-col h-full overflow-hidden gap-2">
+          <RouterLink 
+            v-for="navLink in navLinks"
+            :key="navLink.name"
+            :to="navLink.path"
+            v-slot="{ isActive }"
+            class="flex items-center gap-4 px-3 py-2 rounded-md transition-all ease duration-200 hover:bg-gray-200"
+          >
+            <component :is="navLink.icon" :size="20" :class="isActive ? 'text-emerald-300' : ''" />
+            <p :class="['font-medium text-base', isActive ? 'text-emerald-300' : '']" >{{ navLink.name }}</p>
+          </RouterLink>
+        </nav>
+      </div>
     </Drawer>
     <Button
       unstyled
