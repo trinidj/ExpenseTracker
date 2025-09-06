8db82253554eb7a4431f4e59c8ba00f3d3dbc8b8
trinidj <jm.trinidad199@gmail.com>
2025-09-06 15:49:29 -0400

    feat: add settings route to router config

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,14 @@ import HomeView from "@/views/HomeView.vue";
 import TransactionsView from "@/views/TransactionsView.vue";
 import AnalyticsView from "@/views/AnalyticsView.vue";
 import BudgetsView from "@/views/BudgetsView.vue";
+import SettingsView from "@/views/SettingsView.vue";
 
 const routes = [
   { path: '/', component: HomeView },
   { path: '/transactions', component: TransactionsView },
   { path: '/analytics', component: AnalyticsView },
   { path: '/budgets', component: BudgetsView },
+  { path: '/settings', component: SettingsView },
 ];
 
 const router = createRouter({
