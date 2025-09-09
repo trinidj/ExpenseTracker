7f8828b785799c2c8d5155dae2eb598d16f43a00
trinidj <jm.trinidad199@gmail.com>
2025-09-09 13:16:35 -0400

    refactor: switch PrimeVue theme from Lara to Aura

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './style.css'
 import App from './App.vue'
 import router from '@/router';
 import PrimeVue from 'primevue/config';
-import Lara from '@primeuix/themes/lara';
+import Aura from '@primeuix/themes/aura';
 import { KeyFilter } from 'primevue';
 import { createPinia } from 'pinia';
 import { createPersistedState } from 'pinia-plugin-persistedstate';
@@ -20,7 +20,7 @@ app.directive('keyfilter', KeyFilter);
 app.use(ToastService);
 app.use(PrimeVue, {
   theme: {
-    preset: Lara,
+    preset: Aura,
     options: {
       darkModeSelector: '.dark',
     }
